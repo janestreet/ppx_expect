16e103478cd0fd51c971eb77ff93e434f6c5a487
Thomas Refis <thomas.refis@gmail.com>
2017-05-17 10:12:41 +0100

    v0.9.115.00+14

--- negative-tests/exn.ml
+++ negative-tests/exn.ml
@@ -3,6 +3,6 @@ open! Core
 let%expect_test _ =
   [%expect {| hi ho |}];
   Printexc.record_backtrace false;
-  failwith "hi ho";
+  ignore (failwith "hi ho");
   [%expect {| it's off to work we go |}]
 ;;
