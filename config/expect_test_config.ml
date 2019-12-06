fc6b730d8788c80fa3d443e332fcae255b2f41b4
Xavier Clerc <xclerc@janestreet.com>
2019-12-06 13:03:08 +0000

    v0.14-preview.121.15+243

--- config/expect_test_config.ml
+++ config/expect_test_config.ml
@@ -1,8 +1,8 @@
+module type S = Expect_test_config_intf.S
+module type Expect_test_config = Expect_test_config_intf.Expect_test_config
+
 module Upon_unreleasable_issue = struct
-  type t =
-    [ `CR (** Leaves a CR, so that features cannot be released. *)
-    | `Warning_for_collector_testing (** Only for ppx_expect testing; do not use. *)
-    ]
+  include Expect_test_config_intf.Upon_unreleasable_issue
 
   let equal t1 t2 = t1 = t2
 
@@ -21,25 +21,6 @@ module Upon_unreleasable_issue = struct
 |}
       (comment_prefix t)
   ;;
-end
-
-module type S = sig
-  module IO_run : sig
-    type 'a t
-  end
-
-  module IO_flush : sig
-    type 'a t
-
-    val return : 'a -> 'a t
-    val bind : 'a t -> f:('a -> 'b t) -> 'b t
-    val to_run : 'a t -> 'a IO_run.t
-  end
-
-  val flush : unit -> unit IO_flush.t
-  val run : (unit -> unit IO_run.t) -> unit
-  val flushed : unit -> bool
-  val upon_unreleasable_issue : [ `CR | `Warning_for_collector_testing ]
 end
 
 module IO_run = struct
