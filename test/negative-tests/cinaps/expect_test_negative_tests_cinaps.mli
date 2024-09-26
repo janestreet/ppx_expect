c754ca9ee93ce689feb248820209c27c13fc32e5
public-release <as-public-release@janestreet.com>
2024-09-26 19:08:47 +0100

    v0.18~preview.129.42+498

--- test/negative-tests/cinaps/expect_test_negative_tests_cinaps.mli
+++
@@ -1,8 +0,0 @@
-open! Base
-
-val generate
-  :  postprocess_test_output:string option
-  -> set_up_for_tests:string option
-  -> inline_test_args:string option
-  -> exclude_targets:string list
-  -> unit
